003b80a6a187a43986eb9ff9ae5a3b97b6a9e327
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-04-08 18:05:09 -0300

    Return the Sum of Two Numbers

---
+++ very-easy/1.js
@@ -0,0 +1,7 @@
+// Create a function that takes two numbers as arguments and returns their sum.
+
+const addition = (a, b) => a + b
+
+console.log(addition(3, 2))
+console.log(addition(-3, -6))
+console.log(addition(7, 3))
