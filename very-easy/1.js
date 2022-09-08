2706330d0cf9ce35d110279feb9b097985028e90
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-08 18:00:21 -0300

    Instalação e configuração do Jest

--- very-easy/1.js
+++
@@ -1,7 +0,0 @@
-// Create a function that takes two numbers as arguments and returns their sum.
-
-const addition = (a, b) => a + b
-
-console.log(addition(3, 2))
-console.log(addition(-3, -6))
-console.log(addition(7, 3))
--- very-easy/2.js
+++
@@ -1,7 +0,0 @@
-// Write a function that takes an integer minutes and converts it to seconds.
-
-const convert = (minutes) => minutes * 60
-
-console.log(convert(5))
-console.log(convert(3))
-console.log(convert(2))
