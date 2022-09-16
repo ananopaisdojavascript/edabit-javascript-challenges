67dd32cffc4c6ddc8c34bafdd4b78974a1f4dd6d
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-15 21:52:11 -0300

    Return Something to Me!

---
+++ return-something-to-me.js
@@ -0,0 +1,11 @@
+const giveMeSomething = (a) => {
+    const something = "something";
+    return something + " " + a;
+}
+
+console.log(giveMeSomething("is better than nothing"))
+console.log(giveMeSomething("Bob Jane"))
+console.log(giveMeSomething("something"))
+console.log(giveMeSomething("a"))
+console.log(giveMeSomething("is cooking"))
+console.log(giveMeSomething(" is cooking"))
