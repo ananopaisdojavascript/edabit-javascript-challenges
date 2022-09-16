1b3583ec7d3bd26028a6ba8587fad0cb1974b760
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-15 21:45:32 -0300

    Return the Sum of Two Numbers

--- README.md
+++
@@ -1,3 +0,0 @@
-# Edabit - Desafios de Javascript
-
-Resolução de alguns exercícios do ***site*** Edabit
---
+++ return-the-sum-of-two-numbers.js
@@ -0,0 +1,6 @@
+const addition = (a, b) => a + b;
+
+console.log(addition(2, 3))
+console.log(addition(-3, -6))
+console.log(addition(7, 3))
+console.log(addition(88, 2))
