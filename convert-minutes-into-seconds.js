d42b5ed5d4d0d64bc529a466385fb07608d97bf2
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-20 21:17:48 -0300

    Convert Minutes into Seconds

---
+++ convert-minutes-into-seconds.js
@@ -0,0 +1,11 @@
+const convert = (minutes) => {
+    return minutes * 60;
+}
+
+console.log(convert(5))
+console.log(convert(3))
+console.log(convert(2))
+console.log(convert(6))
+console.log(convert(4))
+console.log(convert(8))
+console.log(convert(60))
