8a2756d657366d11fdb67a9908e71309cf71afae
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-20 21:26:23 -0300

    Area of a Triangle

---
+++ area-of-a-triangle.js
@@ -0,0 +1,8 @@
+const triArea = (base, height) => (base * height) / 2;
+
+console.log(triArea(3, 2))
+console.log(triArea(7, 4))
+console.log(triArea(10, 10))
+console.log(triArea(5, 4))
+console.log(triArea(0, 60))
+console.log(triArea(12, 11))
