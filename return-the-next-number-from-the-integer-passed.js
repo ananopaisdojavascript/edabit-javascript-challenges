aefb8e20d39e1da7bcbfc5c83337ecc2c550f919
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-20 21:23:01 -0300

    Return the Next Number from the Integer Passed

---
+++ return-the-next-number-from-the-integer-passed.js
@@ -0,0 +1,11 @@
+const addition = (num) => {
+    return num + 1;
+}
+
+console.log(addition(0))
+console.log(addition(9))
+console.log(addition(-3))
+console.log(addition(2))
+console.log(addition(-9))
+console.log(addition(999))
+console.log(addition(73))
