69cdac9c9e1050e7847c5274080abf8c764487a9
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:01:17 -0300

    Power Calculator

---
+++ power-calculator.js
@@ -0,0 +1,7 @@
+const circuitPower = (voltage, current) => {
+  return voltage * current;
+};
+
+console.log(circuitPower(110, 3));
+console.log(circuitPower(230, 10));
+console.log(circuitPower(480, 20));
