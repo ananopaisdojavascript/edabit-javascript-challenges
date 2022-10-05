f0d45a17b2830ecb4113602f404dd564c6dbc0d8
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:39:53 -0300

    Buggy Code (Part 5)

---
+++ buggy-code-five.js
@@ -0,0 +1,13 @@
+const printArray = (number) => {
+  const newArray = [];
+
+  for (let i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+
+  return newArray;
+};
+
+console.log(printArray(1));
+console.log(printArray(3));
+console.log(printArray(6));
