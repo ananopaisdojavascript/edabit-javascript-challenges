103082ae6bfcd3e6218e42fdeaf4f8c9bd71070c
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:05:17 -0300

    Convert Hours into Seconds

---
+++ convert-hours-into-seconds.js
@@ -0,0 +1,8 @@
+const howManySeconds = (hours) => {
+  return hours * 3600;
+};
+
+console.log(howManySeconds(2));
+console.log(howManySeconds(10));
+console.log(howManySeconds(24));
+console.log(howManySeconds(36));
