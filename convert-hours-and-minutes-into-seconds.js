e9700243e1e3d856331915f92dc657d7c0b38150
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 22:06:34 -0300

    Convert Hours and Minutes into Seconds

---
+++ convert-hours-and-minutes-into-seconds.js
@@ -0,0 +1,8 @@
+const convert = (hours, minutes) => {
+  return hours * 3600 + minutes * 60;
+};
+
+console.log(convert(1, 3));
+console.log(convert(2, 0));
+console.log(convert(0, 0));
+console.log(convert(0, 30));
