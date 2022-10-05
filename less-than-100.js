bf04a54d87cc4cd162de32a9d2c95691b8072938
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:37:13 -0300

    Less Than 100?

---
+++ less-than-100.js
@@ -0,0 +1,8 @@
+const lessThan100 = (a, b) => {
+  const sum = a + b;
+  return sum < 100;
+};
+
+console.log(lessThan100(22, 15));
+console.log(lessThan100(83, 34));
+console.log(lessThan100(3, 77));
