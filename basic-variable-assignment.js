b40804f17ac86faeb6de7fde7fadcc5fd5f7acd1
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:33:52 -0300

    Basic Variable Assignment

---
+++ basic-variable-assignment.js
@@ -0,0 +1,9 @@
+const nameString = (name) => {
+  let b = "Edabit";
+  let result = name + b;
+  return result;
+};
+
+console.log(nameString("Mubashir"));
+console.log(nameString("Matt"));
+console.log(nameString("javaScript"));
