0d6bf804dd1c464919fdd8886bb64855e10219d9
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 20:58:45 -0300

    Buggy Code (Part 4)

---
+++ buggy-code-four.js
@@ -0,0 +1,7 @@
+const greeting = (name) => {
+  return name === "Mubashir" ? "Hello, my Love!" : "Hello, " + name + "!";
+};
+
+console.log(greeting("Matt"));
+console.log(greeting("Helen"));
+console.log(greeting("Mubashir"));
