81260764a4fa7ed0483dea206d409b854ba2d9f2
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:31:08 -0300

    Basketball Points

---
+++ basketball-points.js
@@ -0,0 +1,11 @@
+const points = (twoPointers, threePointers) => {
+  return 2 * twoPointers + 3 * threePointers;
+};
+
+console.log(points(1, 1));
+console.log(points(7, 5));
+console.log(points(38, 8));
+console.log(points(1, 2));
+console.log(points(2, 1));
+console.log(points(2, 2));
+console.log(points(69, 420));
