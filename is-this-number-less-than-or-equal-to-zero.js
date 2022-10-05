9c650740ef847366813f71f707b2343bcfa0bafe
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:23:24 -0300

    Is the Number Less than or Equal to Zero?

---
+++ is-this-number-less-than-or-equal-to-zero.js
@@ -0,0 +1,7 @@
+const lessThanOrEqualToZero = (num) => {
+  return num <= 0;
+};
+
+console.log(lessThanOrEqualToZero(5));
+console.log(lessThanOrEqualToZero(0));
+console.log(lessThanOrEqualToZero(-2));
