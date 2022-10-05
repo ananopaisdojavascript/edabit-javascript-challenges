8b3ef8ab21edb6b83c658f100e590b6c01c1025c
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-04 21:22:50 -0300

    Correct the Mistakes

---
+++ correct-the-mistakes.js
@@ -0,0 +1,9 @@
+const squared = (b) => {
+  return b * b;
+};
+
+console.log(squared(10));
+console.log(squared(69));
+console.log(squared(666));
+console.log(squared(-21));
+console.log(squared(21));
