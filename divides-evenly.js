4eb12034477d5225c92c2cbd25b79c5476d5e03a
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 21:46:28 -0300

    Divides Evenly

---
+++ divides-evenly.js
@@ -0,0 +1,13 @@
+const dividesEvenly = (a, b) => {
+  return a % b === 0;
+};
+
+console.log(dividesEvenly(98, 7));
+console.log(dividesEvenly(87, 49));
+console.log(dividesEvenly(34, 14));
+console.log(dividesEvenly(78, 6));
+console.log(dividesEvenly(30, 4));
+console.log(dividesEvenly(87, 73));
+console.log(dividesEvenly(74, 7));
+console.log(dividesEvenly(87, 29));
+console.log(dividesEvenly(48, 24));
