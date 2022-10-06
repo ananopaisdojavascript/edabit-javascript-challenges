9014402b52f54c6435448d562e77bc18055559d0
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 21:21:43 -0300

    Compare Strings by Count of Characters

---
+++ compare-strings-by-count-of-characters.js
@@ -0,0 +1,11 @@
+const comp = (str1, str2) => {
+  return str1.length === str2.length;
+};
+
+console.log(comp("AB", "CD"));
+console.log(comp("ABC", "DE"));
+console.log(comp("hello", "edabit"));
+console.log(comp("meow", "woof"));
+console.log(comp("jrnvjrnnt", "cvjknfjvmfvnfjn"));
+console.log(comp("jkvnjrt", "krnf"));
+console.log(comp("Facebook", "Snapchat"));
