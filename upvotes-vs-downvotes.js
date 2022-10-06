2200d143710fa42902a032885107045832d2b142
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-06 20:28:30 -0300

    Upvotes vs Downvotes

---
+++ upvotes-vs-downvotes.js
@@ -0,0 +1,7 @@
+const getVoteCount = (votes) => {
+  return votes.upvotes - votes.downvotes;
+};
+
+console.log(getVoteCount({ upvotes: 13, downvotes: 0 }));
+console.log(getVoteCount({ upvotes: 2, downvotes: 33 }));
+console.log(getVoteCount({ upvotes: 132, downvotes: 132 }));
