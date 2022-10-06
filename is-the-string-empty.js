dc84e5c8d56b86804c3d841d38e8d407798f23c4
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 21:28:37 -0300

    Is the String Empty?

---
+++ is-the-string-empty.js
@@ -0,0 +1,10 @@
+const isEmpty = (s) => {
+  return s.length === 0;
+};
+
+console.log(isEmpty(""));
+console.log(isEmpty(" "));
+console.log(isEmpty("a"));
+console.log(isEmpty("            "));
+console.log(isEmpty("38215"));
+console.log(isEmpty("afjabsdf"));
