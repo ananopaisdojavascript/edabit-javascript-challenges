190b4b2440474c3d816937a782b330e429ee856f
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 21:41:56 -0300

    Recursion: Length of a String

---
+++ recursion-length-of-a-string.js
@@ -0,0 +1,10 @@
+const length = (str) => {
+  return str.length;
+  length(str);
+};
+
+console.log(length("shipment"));
+console.log(length("apple"));
+console.log(length("make"));
+console.log(length("a"));
+console.log(length(""));
