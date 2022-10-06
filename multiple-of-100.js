245c6866860eed63ec01cc1325ce849c23cc3df9
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 21:36:08 -0300

    Multiple of 100

---
+++ multiple-of-100.js
@@ -0,0 +1,11 @@
+const divisible = (num) => {
+  return num % 100 === 0;
+};
+
+console.log(divisible(1));
+console.log(divisible(100));
+console.log(divisible(1000));
+console.log(divisible(111000));
+console.log(divisible(-1));
+console.log(divisible(0));
+console.log(divisible(-100));
