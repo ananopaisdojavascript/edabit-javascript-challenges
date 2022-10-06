5e5d611e1842b280313836b9f31320e566731249
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 22:00:59 -0300

    Minimal I: If Boolean Then Boolean

---
+++ minimal-I-if-boolean-then-boolean.js
@@ -0,0 +1,12 @@
+const isEven = (n) => {
+  return n % 2 === 0;
+};
+
+console.log(isEven(2));
+console.log(isEven(3));
+console.log(isEven(10));
+console.log(isEven(31));
+console.log(isEven(666));
+console.log(isEven(777));
+console.log(isEven(3482034));
+console.log(isEven(3482035));
