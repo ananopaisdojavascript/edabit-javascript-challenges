8f89f57393ae20b1ae42ab21b2ae0584507eb769
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-06 20:43:34 -0300

    Fix the Bug: Simple Array Manipulation

---
+++ fix-the-bug-simple-array-manipulation.js
@@ -0,0 +1,8 @@
+const incrementItems = (arr) => {
+  for (let i = 0; i < arr.length; i++) arr[i] === arr[i]++;
+  return arr;
+};
+
+console.log(incrementItems([0, 1, 2, 3]));
+console.log(incrementItems([2, 4, 6, 8]));
+console.log(incrementItems([-1, -2, -3, -4]));
