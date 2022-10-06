00c400e16bdc4694124ef3487f6ceaad7a9991dc
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 22:06:10 -0300

    Concatenate First and Last Name into One String

---
+++ concatenate-first-and-last-name-onto-one-string.js
@@ -0,0 +1,7 @@
+const concatName = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
+};
+
+console.log(concatName("First", "Last"));
+console.log(concatName("John", "Doe"));
+console.log(concatName("Mary", "Jane"));
