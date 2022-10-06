83e59d1ccf40713f7b96ac6086c6fd0723a2923a
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-05 21:32:26 -0300

    Check if an Integer is Divisible By Five

---
+++ check-if-an-integer-is-divisble-by-five.js
@@ -0,0 +1,11 @@
+const divisibleByFive = (n) => {
+  return n % 5 === 0;
+};
+
+console.log(divisibleByFive(7));
+console.log(divisibleByFive(5));
+console.log(divisibleByFive(15));
+console.log(divisibleByFive(33));
+console.log(divisibleByFive(-18));
+console.log(divisibleByFive(999));
+console.log(divisibleByFive(2));
