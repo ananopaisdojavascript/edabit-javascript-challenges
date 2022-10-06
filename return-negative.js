91867cb7fb03962c418e3b3a335e19407179c540
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-06 20:32:22 -0300

    Return Negative

---
+++ return-negative.js
@@ -0,0 +1,8 @@
+const returnNegative = (n) => {
+  return n <= 0 ? n : -n;
+};
+
+console.log(returnNegative(4));
+console.log(returnNegative(15));
+console.log(returnNegative(-4));
+console.log(returnNegative(0));
