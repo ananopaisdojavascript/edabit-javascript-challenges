6d00ed3bd2f59c72c396b82049e676841eb7e6ce
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-06 21:14:56 -0300

    Is the String Odd or Even?

---
+++ is-the-string-odd-or-even.js
@@ -0,0 +1,10 @@
+const oddOrEven = (s) => {
+  return s.length % 2 === 0;
+};
+
+console.log(oddOrEven("apples"));
+console.log(oddOrEven("banana"));
+console.log(oddOrEven("cherry"));
+console.log(oddOrEven("mango"));
+console.log(oddOrEven("peach"));
+console.log(oddOrEven("pears"));
