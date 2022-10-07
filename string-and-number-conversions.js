eb79b58ed0e32e837f1f322aed682062cbe06830
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-07 20:29:46 -0300

    String and Number Conversions

---
+++ string-and-number-conversions.js
@@ -0,0 +1,12 @@
+const intToString = (num) => String(num);
+
+const stringToInt = (num) => parseInt(num);
+
+console.log(intToString(4));
+console.log(intToString(65));
+console.log(intToString(29348));
+console.log(intToString(49583908545));
+console.log(stringToInt("4"));
+console.log(stringToInt("65"));
+console.log(stringToInt("29348"));
+console.log(stringToInt("49583908545"));
