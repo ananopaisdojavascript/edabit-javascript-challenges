be2abe895f2554ba8b2a82cc21300b854dd9dd2e
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-06 21:35:55 -0300

    Century Crisis

---
+++ century-crisis.js
@@ -0,0 +1,7 @@
+const futurePeople = (population, n) => {
+  return population + n * 360;
+};
+
+console.log(futurePeople(256, 2));
+console.log(futurePeople(3248, 6));
+console.log(futurePeople(5240, 3));
