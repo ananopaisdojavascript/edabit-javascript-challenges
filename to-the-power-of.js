b23bb44cb1f911e5467200039307ecb22e1668ae
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-06 21:48:27 -0300

    To the Power of _____

---
+++ to-the-power-of.js
@@ -0,0 +1,7 @@
+const calculateExponent = (num, exp) => {
+  return Math.pow(num, exp);
+};
+
+console.log(calculateExponent(5, 5));
+console.log(calculateExponent(10, 10));
+console.log(calculateExponent(3, 3));
