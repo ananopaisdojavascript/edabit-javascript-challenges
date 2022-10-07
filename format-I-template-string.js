7ec32d8d3d5a3a5670d3f5eb219e8df59ffd6927
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-06 21:00:34 -0300

    Format I: Template String

---
+++ format-I-template-string.js
@@ -0,0 +1,9 @@
+const format = (a, b, c) => {
+  const template = `Their names were: ${a}, ${b} and ${c}.`;
+  return template;
+};
+
+console.log(format("John", "Joe", "Jack"));
+console.log(format("Peter", "Pin", "Pan"));
+console.log(format("E", "Da", "Bit"));
+console.log(format("Bulbasaur", "Charmander", "Squirtle"));
