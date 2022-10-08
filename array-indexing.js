8abd8fe88e4c80440f2e9080a59000fe2f522a5c
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-07 21:49:44 -0300

    Array Indexing

---
+++ array-indexing.js
@@ -0,0 +1,5 @@
+const valueAt = (arr, i) => arr[Math.floor(i)];
+
+console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2));
+console.log(valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2));
+console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2));
