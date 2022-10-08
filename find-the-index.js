c15bc7aefc827111007cb6172144e9c1e938bbdf
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-07 21:42:10 -0300

    Find the Index

---
+++ find-the-index.js
@@ -0,0 +1,6 @@
+const findIndex = (arr, str) => arr.indexOf(str);
+
+console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh"));
+console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue"));
+console.log(findIndex(["a", "g", "y", "d"], "d"));
+console.log(findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple"));
