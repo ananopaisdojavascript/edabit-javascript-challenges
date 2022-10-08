fef21441a29e50a7b0eb324eb5154726e2290577
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-07 21:33:24 -0300

    Convert an Array to a String

---
+++ convert-an-array-to-string.js
@@ -0,0 +1,5 @@
+const arrayToString = (arr) => arr.join("");
+
+console.log(arrayToString([1, 2, 3, 4, 5, 6]));
+console.log(arrayToString(["a", "b", "c", "d", "e", "f"]));
+console.log(arrayToString([1, 2, 3, "a", "s", "dAAAA"]));
