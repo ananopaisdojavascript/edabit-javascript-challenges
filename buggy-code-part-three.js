7eeb1a2d2ff3fffcd48f114f34cd8a9befb48f3e
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-10 20:32:59 -0300

    Buggy Code (Part 3)

---
+++ buggy-code-part-three.js
@@ -0,0 +1,11 @@
+const sumArray = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+
+console.log(sumArray([1, 2, 3, 4, 5]));
+console.log(sumArray([-1, 0, 1]));
+console.log(sumArray([0, 4, 8, 12]));
