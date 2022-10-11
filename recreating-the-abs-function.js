a4f065085d68c6067660b278b9ba7bf8215111f0
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-11 18:44:42 -0300

    Recreating the abs() Function

---
+++ recreating-the-abs-function.js
@@ -0,0 +1,10 @@
+const absolute = (n) => {
+  return n < 0 ? n * -1 : n;
+};
+
+console.log(absolute(-1.21719794));
+console.log(absolute(-12.132));
+console.log(absolute(-544.0));
+console.log(absolute(4666));
+console.log(absolute(-3.14));
+console.log(absolute(250));
