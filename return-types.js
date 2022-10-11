afe9566db87d43a11b3df33cc1e8c749acd02bbf
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-10 21:26:59 -0300

    Return Types

---
+++ return-types.js
@@ -0,0 +1,5 @@
+const arrayValuesTypes = (arr) => arr.map(item => typeof item);
+
+console.log(arrayValuesTypes([1, 2, "null", []]));
+console.log(arrayValuesTypes(["214", true, false, 2, 2.15, [], null]));
+console.log(arrayValuesTypes([21.1, "float", "array", ["I am array"], null, true, 214])); 
