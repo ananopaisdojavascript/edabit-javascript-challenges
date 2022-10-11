19c1e3068c062cffbd340d16b3a8ff7448790bb3
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-11 20:29:51 -0300

    Reverse Psychology

---
+++ reverse-psicology.js
@@ -0,0 +1,5 @@
+const reversePsychology = (s) => `Do not ${s}`;
+
+console.log(reversePsychology("wash the dishes"));
+console.log(reversePsychology("eat your lunch"));
+console.log(reversePsychology("go to school"));
