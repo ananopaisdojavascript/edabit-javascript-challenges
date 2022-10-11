35b5f47764d197dc062986616747274e367daf6e
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-11 20:40:29 -0300

    Convert All Array Items to String

---
+++ convert-all-array-items-to-string.js
@@ -0,0 +1,8 @@
+const parseArray = (arr) => {
+  return arr.map((item) => item.toString());
+};
+
+console.log(parseArray([1, 2, "a", "b"]));
+console.log(parseArray(["abc", 123, "def", 456]));
+console.log(parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]));
+console.log(parseArray([]));
