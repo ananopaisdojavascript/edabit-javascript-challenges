bb52ca5ee2f47cff81c60317e903557f4bb95833
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-11 18:49:17 -0300

    Is the Last Character an "N"?

---
+++ is-the-last-character-a-n.js
@@ -0,0 +1,6 @@
+const isLastCharacterN = (word) => word.endsWith("n");
+
+console.log(isLastCharacterN("Aiden"));
+console.log(isLastCharacterN("Piet"));
+console.log(isLastCharacterN("Bert"));
+console.log(isLastCharacterN("Dean"));
