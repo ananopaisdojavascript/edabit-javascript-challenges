cf7f82211f4aac06e7b4978bb2ef415b6d5b14cb
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-11 21:05:15 -0300

    Is the Word Singular or Plural?

---
+++ is-this-word-singular-or-plural.js
@@ -0,0 +1,15 @@
+const isPlural = (word) => word.endsWith("s");
+
+console.log(isPlural("changes"));
+console.log(isPlural("change"));
+console.log(isPlural("dudes"));
+console.log(isPlural("magic"));
+console.log(isPlural("flowers"));
+console.log(isPlural("varies"));
+console.log(isPlural("checks"));
+console.log(isPlural("mood"));
+console.log(isPlural("whiteboard"));
+console.log(isPlural("cow"));
+console.log(isPlural("word"));
+console.log(isPlural("love"));
+console.log(isPlural("silly"));
