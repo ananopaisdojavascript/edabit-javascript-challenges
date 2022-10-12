f63336a9c915d8659409aaca50ec057f866fb360
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-12 18:06:59 -0300

    Taxi Journey

---
+++ taxi-journey.js
@@ -0,0 +1,9 @@
+const journeyDistance = (num) => {
+  return Math.floor(num / 2);
+};
+
+console.log(journeyDistance(3));
+console.log(journeyDistance(9));
+console.log(journeyDistance(5));
+console.log(journeyDistance(0));
+console.log(journeyDistance(15));
