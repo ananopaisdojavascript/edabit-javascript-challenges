1f0f670dfdcdcf7d093b32d37786a213d7c29306
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-13 20:43:04 -0300

    Array of Word Lengths

---
+++ array-of-word-lengths.js
@@ -0,0 +1,7 @@
+const wordLengths = (arr) => arr.map(word => word.length);
+
+console.log(wordLengths(["hello", "world"]));
+console.log(wordLengths(["Halloween", "Thanksgiving", "Christmas"]));
+console.log(wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"]));
+console.log(wordLengths(["Indiana", "Jones", "and", "the", "Temple", "of", "Doom"]));
+console.log(wordLengths(["Programming", "is", "fun"]));
