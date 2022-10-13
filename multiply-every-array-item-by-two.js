5beb5410ff2cbd24db3a943ad89953a0a09ed821
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-13 20:21:18 -0300

    Multiply Every Array Item by Two

---
+++ multiply-every-array-item-by-two.js
@@ -0,0 +1,7 @@
+const getMultipliedArr = (arr) => {
+  return arr.map((item) => item * 2);
+};
+
+console.log(getMultipliedArr([2, 5, 3]));
+console.log(getMultipliedArr([1, 86, -5]));
+console.log(getMultipliedArr([5, 382, 0]));
