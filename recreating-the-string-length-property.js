1977ac1a2ed29ce109e0a596cf5da8b7bbba2bb6
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-13 20:56:57 -0300

    Recreating the String.length Property

---
+++ recreating-the-string-length-property.js
@@ -0,0 +1,11 @@
+const length = (s) => {
+  return s.split("").length;
+};
+
+console.log(length("Hello World"));
+console.log(length("Edabit"));
+console.log(length("wash your hands!"));
+console.log(length("34445"));
+console.log(length("   "));
+console.log(length(""));
+console.log(length("%%$"));
