9c3922de1fddb6867e062168dafb8a32f52c63c3
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-14 17:49:20 -0300

    Left Shift by Powers of Two

---
+++ easy/left-shift-by-powers-of-two.js
@@ -0,0 +1,8 @@
+const shiftToLeft = (x, y) => x * 2 ** y;
+
+console.log(shiftToLeft(5, 2));
+console.log(shiftToLeft(10, 3));
+console.log(shiftToLeft(-32, 2));
+console.log(shiftToLeft(-6, 5));
+console.log(shiftToLeft(12, 4));
+console.log(shiftToLeft(46, 6));
