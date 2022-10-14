57189ecb542c7a47238ff43b6922aa1973c43e93
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-13 21:13:07 -0300

    Bitwise Operations

---
+++ easy/bitwise-operations.js
@@ -0,0 +1,19 @@
+const bitwiseAND = (n1, n2) => {
+  return n1 & n2;
+};
+const bitwiseOR = (n1, n2) => {
+  return n1 | n2;
+};
+const bitwiseXOR = (n1, n2) => {
+  return n1 ^ n2;
+};
+
+console.log(bitwiseAND(7, 12));
+console.log(bitwiseOR(7, 12));
+console.log(bitwiseXOR(7, 12));
+console.log(bitwiseAND(32, 17));
+console.log(bitwiseOR(32, 17));
+console.log(bitwiseXOR(32, 17));
+console.log(bitwiseAND(13, 19));
+console.log(bitwiseOR(13, 19));
+console.log(bitwiseXOR(13, 19));
