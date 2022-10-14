dd207b64f21142a37d7a0e2398bb7f2b13fde7fe
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-14 19:02:05 -0300

    Convert a Number to Base-2

---
+++ easy/convert-a-number-to-base-2.js
@@ -0,0 +1,12 @@
+const binary = (decimal) => {
+  return Number(decimal).toString(2);
+};
+
+console.log(binary(100));
+console.log(binary(1));
+console.log(binary(0));
+console.log(binary(69));
+console.log(binary(1023));
+console.log(binary(511));
+console.log(binary(666));
+console.log(binary(123));
