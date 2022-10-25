15b3543ad32bee4aef0492da29dcd7a98a43eb98
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-10-25 20:28:38 -0300

    Spaces Between Each Character

---
+++ very-easy/spaces-between-each-character.js
@@ -0,0 +1,10 @@
+const spaceMeOut = (str) => {
+  return str.split("").join(" ");
+};
+
+console.log(spaceMeOut("space"));
+console.log(spaceMeOut("far out"));
+console.log(spaceMeOut("elongated musk"));
+console.log(spaceMeOut("long"));
+console.log(spaceMeOut("123"));
+console.log(spaceMeOut("a1b2c3"));
